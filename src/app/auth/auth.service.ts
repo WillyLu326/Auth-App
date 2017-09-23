3b9a66262c5d4938c18697896c4d771bcaf2b054
WillyLu326 <luzh0326@gmail.com>
2017-09-22 22:48:21 -0400

    import auth0 in authService

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
+import auth0 from 'auth0-js';
 
 @Injectable()
 export class AuthService {
