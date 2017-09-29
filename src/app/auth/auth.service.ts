582f21c2a168bc72b25b9bc9dc6c5149ff019498
WillyLu326 <luzh0326@gmail.com>
2017-09-29 17:25:19 -0400

    create login button and add login function

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,7 +7,7 @@ import auth0 from 'auth0-js';
 export class AuthService {
     
     auth0 = new auth0.WebAuth({
-        clientId: 'earBAaq-hjrVHGOt6BB2FmiILklKas89',
+        clientID: 'earBAaq-hjrVHGOt6BB2FmiILklKas89',
         domain: 'willy-project.auth0.com',
         redirectUri: 'http://localhost:4200',
         responseType: 'token id_token'
